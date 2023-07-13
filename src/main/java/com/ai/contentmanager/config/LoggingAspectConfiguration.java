7733ec94326cd57779e078d32e7b9a68167e4b48
Sumit <sumit.esri@gmail.com>
2023-07-13 14:54:01 +0530

    initial commit

---
+++ src/main/java/com/ai/contentmanager/config/LoggingAspectConfiguration.java
@@ -0,0 +1,17 @@
+package com.ai.contentmanager.config;
+
+import com.ai.contentmanager.aop.logging.LoggingAspect;
+import org.springframework.context.annotation.*;
+import org.springframework.core.env.Environment;
+import tech.jhipster.config.JHipsterConstants;
+
+@Configuration
+@EnableAspectJAutoProxy
+public class LoggingAspectConfiguration {
+
+    @Bean
+    @Profile(JHipsterConstants.SPRING_PROFILE_DEVELOPMENT)
+    public LoggingAspect loggingAspect(Environment env) {
+        return new LoggingAspect(env);
+    }
+}
---
+++ src/main/java/com/ai/contentmanager/service/dto/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Data Transfer Objects.
+ */
+package com.ai.contentmanager.service.dto;
